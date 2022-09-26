b272034e9847a9774311f4d58f81ca35d4dca192
Karan <kbir@salientprocess.com>
2022-09-26 19:22:43 +0530

    Added 3 new Files

---
+++ Infrastructure/Interface/ITeamService.cs
@@ -0,0 +1,18 @@
+﻿using SchedulingApplication.Data.Entities;
+using SchedulingApplication.Models;
+
+namespace SchedulingApplication.Infrastructure.Interface
+{
+	public interface ITeamService
+	{
+		List<Coach> GetAllCoach();
+		List<Player> GetAllPlayers();
+		JqueryDataTablesResult<TeamModel> GetAllTeams(JqueryDataTablesParameters request);
+		Task<bool> AddTeam(Team team);
+		Team? TeamDetails(int TeamId);
+		List<Coach> GetCoachForTeam();
+		List<Player> GetPlayersByTeamId(int TeamId);
+		Task<bool> DeleteTeamById(int TeamId);
+		Task<bool> DeleteMultipleTeams(List<int> teamIds);
+	}
+}
