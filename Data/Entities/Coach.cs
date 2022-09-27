f411d17f820e7f5ca5b0c027b8cc9fc870833c57
st-dotnet <stdev.net@gmail.com>
2022-09-27 23:18:58 +0530

    Revert "Implemented the Add Coach Image functionality in the Project"

--- Data/Entities/Coach.cs
+++ Data/Entities/Coach.cs
@@ -8,12 +8,12 @@ namespace SchedulingApplication.Data.Entities
     {
         [Key]
         public int Id { get; set; }
+        public string? Image { get; set; }
         public string? Name { get; set; }
         public string? EmailAddress { get; set; }
         public DateTime? CreatedOn { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? UpdatedOn { get; set; }
         public int? UpdatedBy { get; set; }
-        public string? Image { get; set; }
     }
 }
--- wwwroot/assets/js/custom/apps/coaches/addCoach.js
+++ wwwroot/assets/js/custom/apps/coaches/addCoach.js
@@ -4,6 +4,71 @@ var KTModalCoachesAdd = function () {
     return {
         init: function () {
             i = new bootstrap.Modal(document.querySelector("#modal_add_coach")), r = document.querySelector("#modal_add_coach_form"), t = r.querySelector("#modal_add_coach_submit"), e = r.querySelector("#modal_add_coach_cancel"), o = r.querySelector("#modal_add_coach_close"), n = FormValidation.formValidation(r, {
+                fields: {
+                    name: {
+                        validators: {
+                            notEmpty: {
+                                message: "Coach name is required"
+                            }
+                        }
+                    },
+                    email: {
+                        validators: {
+                            notEmpty: {
+                                message: "Coach email is required"
+                            }
+                        }
+                    },
+                    "first-name": {
+                        validators: {
+                            notEmpty: {
+                                message: "First name is required"
+                            }
+                        }
+                    },
+                    "last-name": {
+                        validators: {
+                            notEmpty: {
+                                message: "Last name is required"
+                            }
+                        }
+                    },
+                    country: {
+                        validators: {
+                            notEmpty: {
+                                message: "Country is required"
+                            }
+                        }
+                    },
+                    address1: {
+                        validators: {
+                            notEmpty: {
+                                message: "Address 1 is required"
+                            }
+                        }
+                    },
+                    city: {
+                        validators: {
+                            notEmpty: {
+                                message: "City is required"
+                            }
+                        }
+                    },
+                    state: {
+                        validators: {
+                            notEmpty: {
+                                message: "State is required"
+                            }
+                        }
+                    },
+                    postcode: {
+                        validators: {
+                            notEmpty: {
+                                message: "Postcode is required"
+                            }
+                        }
+                    }
+                },
                 plugins: {
                     trigger: new FormValidation.plugins.Trigger,
                     bootstrap: new FormValidation.plugins.Bootstrap5({
@@ -16,59 +81,41 @@ var KTModalCoachesAdd = function () {
                 n.revalidateField("country")
             })), t.addEventListener("click", (function (e) {
                 e.preventDefault(), n && n.validate().then((function (e) {
-
-                    var formData = new FormData($('#modal_add_coach_form')[0]);
-
-                    formData.append('baseImage', $('input[type=file]')[0].files[0]);
-                    formData.append('name', $('#name').val());
-                    formData.append('emailAddress', $('#emailAddress').val());
-                    console.log(formData);
-
-
-
-                    /*var postCoach = {
-                        image: $('input[type=file]').val(),
+                    console.log("Submit Button Clicked");
+                    var postCoach = {
                         name: $('#Name').val(),
                         emailaddress: $('#EmailAddress').val(),
-                    };*/
-
-
+                    };
+                    console.log(postCoach);
                     $.ajax({
                         type: "POST",
                         url: "/Coach/AddCoach",
-                        data: formData,
-                        contentType: false,
-                        processData: false,
-                        /*dataType: "json",*/
+                        data: postCoach,
+                        dataType: "json",
                         success: function (response) {
-                            if (response.success) {
-                                console.log(response);
-                            }
-                        }
-
-                    }),
-
-                        console.log("validated!"), "Valid" == e ? (t.setAttribute("data-kt-indicator", "on"), t.disabled = !0, setTimeout((function () {
-                            t.removeAttribute("data-kt-indicator"), Swal.fire({
-                                text: "Form has been successfully submitted!",
-                                icon: "success",
+                            console.log("validated!"), response.success ? (t.setAttribute("data-kt-indicator", "on"), t.disabled = !0, setTimeout((function () {
+                                t.removeAttribute("data-kt-indicator"), Swal.fire({
+                                    text: "Form has been successfully submitted!",
+                                    icon: "success",
+                                    buttonsStyling: !1,
+                                    confirmButtonText: "Ok, got it!",
+                                    customClass: {
+                                        confirmButton: "btn btn-primary"
+                                    }
+                                }).then((function (e) {
+                                    e.isConfirmed && (i.hide(), t.disabled = !1, window.location = "/Coach/Index")
+                                }))
+                            }), 2e3)) : Swal.fire({
+                                text: "Sorry, looks like there are some errors detected, please try again.",
+                                icon: "error",
                                 buttonsStyling: !1,
                                 confirmButtonText: "Ok, got it!",
                                 customClass: {
                                     confirmButton: "btn btn-primary"
                                 }
-                            }).then((function (e) {
-                                e.isConfirmed && (i.hide(), t.disabled = !1, window.location = "/Coach/Index")
-                            }))
-                        }), 2e3)) : Swal.fire({
-                            text: "Sorry, looks like there are some errors detected, please try again.",
-                            icon: "error",
-                            buttonsStyling: !1,
-                            confirmButtonText: "Ok, got it!",
-                            customClass: {
-                                confirmButton: "btn btn-primary"
-                            }
-                        })
+                            })
+                        }  
+                    })
                 }))
             })), e.addEventListener("click", (function (t) {
                 t.preventDefault(), Swal.fire({
