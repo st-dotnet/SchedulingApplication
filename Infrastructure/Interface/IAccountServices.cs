f0f43d7ab9bbed9e8fc8f62e8c1cd91541cc6353
stdev <stdev.net@gmail.com>
2022-09-27 23:00:59 +0530

    IAccountServices.cs

---
+++ Infrastructure/Interface/IAccountServices.cs
@@ -0,0 +1,10 @@
+﻿using SchedulingApplication.Models;
+
+namespace SchedulingApplication.Infrastructure.Interface
+{
+    public interface IAccountServices
+    {
+        Task<bool> LogIn(LogInModel model);
+		Task<bool> LogOut();
+	}
+}
