19f64eedfa50974d83adefada473590439a2c450
Karan <kbir@salientprocess.com>
2022-09-30 22:38:46 +0530

    Worked onthe Dashboard Module.

--- Controllers/DashBoardController.cs
+++ Controllers/DashBoardController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SchedulingApplication.Data.Entities;
+using SchedulingApplication.Infrastructure.Interface;
 
 namespace SchedulingApplication.Controllers
 {
     public class DashBoardController : Controller
     {
+        private readonly IPlayerServices _playerServices;
+        private readonly IMapper _mapper;
+        private readonly IDashboardServices _dashboardServices;
+
+        public DashBoardController(IPlayerServices playerServices, IMapper mapper, IDashboardServices dashboardServices)
+        {
+            _playerServices = playerServices;
+            _mapper = mapper;
+            _dashboardServices = dashboardServices; 
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Player = _dashboardServices?.GetAllPlayers();
+            ViewBag.Team = _dashboardServices?.GetAllTeams();
+            ViewBag.coach = _dashboardServices?.GetAllCoach();   
             return View();
         }
+
+        
     }
 }
---
+++ Infrastructure/Services/DashboardServices.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using SchedulingApplication.Data;
+using SchedulingApplication.Data.Entities;
+using SchedulingApplication.Infrastructure.Interface;
+
+namespace SchedulingApplication.Infrastructure.Services
+{
+    public class DashboardServices : IDashboardServices
+    {
+        private readonly SchedulingApplicationContext _dbContext;
+        private readonly IMapper _mapper;
+        public DashboardServices(SchedulingApplicationContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+    
+        public List<Coach> GetAllCoach()
+        {
+            try
+            {
+                var coach = _dbContext.Coaches.ToList();
+                return coach;   
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            try
+            {
+                var player = _dbContext.Players.ToList();
+                return player;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Team> GetAllTeams()
+        {
+            try
+            {
+                var team = _dbContext.Teams.ToList();
+                return team;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
