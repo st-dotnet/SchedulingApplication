9dfd13c90450013f9702e6c03f5321cac538455d
Amit Rana <arana@salientprocess.com>
2022-09-30 19:51:31 +0530

    Worked on the DashBoard Module.

--- Program.cs
+++ Program.cs
@@ -67,16 +67,6 @@ builder.Services.Configure<MailKitEmailSenderOptions>(options =>
 	options.Sender_Name = "Suprem";
 });
 
-//builder.Services.Configure<MailKitEmailSenderOptions>(options =>
-//{
-//	options.Host_Address = configuration["ExternalProviders:MailKit:SMTP:Address"];
-//	options.Host_Port = Convert.ToInt32(configuration["ExternalProviders:MailKit:SMTP:Port"]);
-//	options.Host_Username = configuration["ExternalProviders:MailKit:SMTP:Account"];
-//	options.Host_Password = configuration["ExternalProviders:MailKit:SMTP:Password"];
-//	options.Sender_EMail = configuration["ExternalProviders:MailKit:SMTP:SenderEmail"];
-//	options.Sender_Name = configuration["ExternalProviders:MailKit:SMTP:SenderName"];
-//});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
