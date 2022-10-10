1cb2231566d444f4763ee5c64b8039b2b81ca7f2
Amit Rana <arana@salientprocess.com>
2022-10-10 18:03:41 +0530

    Worked on the Nav bars.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+$(document).ready(function () {
+	$(function () {
+		var current = location.pathname;
+		$('#kt_aside_menu .menu-item a.menu-link').each(function () {
+			var $this = $(this);
+			// if the current path is like this link, make it active
+			if ($this.attr('href').indexOf(current) !== -1) {
+				$this.addClass('active');
+			}
+		})
+	})
+});
